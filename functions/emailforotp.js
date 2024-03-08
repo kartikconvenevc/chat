7f8334ebb04ce86019d7f9d6c15d097a3f00f973
master3511 <prasanna@convenevc.com>
2024-03-08 12:18:26 +0530

    email of enquiry@coherentbs.com

--- functions/emailforotp.js
+++ functions/emailforotp.js
@@ -7,18 +7,18 @@ console.log("the api is get called :",req.body)
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
+    host: "smtp.hostinger.com",
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: "prasanna@convenevc.com", // generated ethereal user
-      pass: "Vihaan@2024" // generated ethereal password
+      user: "enquiry@coherentbs.com", // generated ethereal user
+      pass: "Chat@2024" // generated ethereal password
     }
   })
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: "prasanna@convenevc.com", // sender address
+    from: "enquiry@coherentbs.com", // sender address
     to: req.body.emailid, // list of receivers
     subject: "Email verification for Conevene Chat Application.", // Subject line
     text: `Hello world? `, // plain text body
