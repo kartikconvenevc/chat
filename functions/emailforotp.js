b1a281c50648eae7220ec603370bfa0feaec5b9d
master3511 <prasanna@convenevc.com>
2024-03-08 14:36:24 +0530

    hello @CoherentB .com

--- functions/emailforotp.js
+++ functions/emailforotp.js
@@ -1,35 +1,33 @@
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
 export default async (req, res) => {
-  // Only needed if you don't have a real mail account for testing
-//   let testAccount = await nodemailer.createTestAccount()
-console.log("the api is get called :",req.body)
+  console.log("the api is get called:", req.body);
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: "smtp.hostinger.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
-      user: "enquiry@coherentbs.com", // generated ethereal user
-      pass: "Chat@2024" // generated ethereal password
+      user: "hello@coherentbs.com", // your email address
+      pass: "Chat@2024" // your email password
     }
-  })
+  });
 
   // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: "enquiry@coherentbs.com", // sender address
-    to: req.body.emailid, // list of receivers
-    subject: "Email verification for Conevene Chat Application.", // Subject line
-    text: `Hello world? `, // plain text body
-    html: `<p>The otp for the verification is <b>${req.body.emailotp} </b>. Please enter this in app to verify.Thank you! </p>` // html body
-  })
+  try {
+    let info = await transporter.sendMail({
+      from: "hello@coherentbs.com", // sender address
+      to: req.body.emailid, // list of receivers
+      subject: "Email verification for Conevene Chat Application.", // Subject line
+      text: `Hello world?`, // plain text body
+      html: `<p>The OTP for the verification is <b>${req.body.emailotp}</b>. Please enter this in the app to verify. Thank you!</p>` // html body
+    });
 
-  console.log("Message sent: %s", info.messageId)
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
-
-  res.json("Message sent")
-}
+    console.log("Message sent: %s", info.messageId);
+    res.json("Message sent");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json("Error sending email");
+  }
+};
